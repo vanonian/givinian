03195dad15f3b0f6f353ce5de06a813ebb9254c2
Ishmukhametov Ruslan <ishmukruslan@gmail.com>
2017-11-29 20:47:54 +0300

    Task4

--- task4/solve.go
+++ task4/solve.go
@@ -4,7 +4,7 @@ import "unicode"
 
 func RemoveEven(array []int) []int {
     result := make([]int, 0)
-    for key, val := range array {
+    for _, val := range array {
         if val % 2 == 1 {
             result = append(result, val)
         }
@@ -24,7 +24,7 @@ func DifferentWordsCount(str string) int {
     dict := make(map[string]bool)
     word := ""
     result := 0
-    for key, char := range (x + " ") {
+    for _, char := range (str + " ") {
         if unicode.IsLetter(char) {
             word += string(unicode.ToLower(char))
         } else if word != "" {
