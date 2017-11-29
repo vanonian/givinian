7cc3278439686bd869df159b113ac68ec686f8ff
Ishmukhametov Ruslan <ishmukruslan@gmail.com>
2017-11-29 20:40:16 +0300

    Task 4

---
+++ task4/main/solve.go
@@ -0,0 +1,39 @@
+package main
+
+import "unicode"
+
+func RemoveEven(array []int) []int {
+    result := make([]int, 0)
+    for key, val := range array {
+        if val % 2 == 1 {
+            result = append(result, val)
+        }
+    }
+    return result
+}
+
+func PowerGenerator(base int) func() int {
+    start := 1
+    return func() int {
+        start *= base
+        return start
+    }
+}
+
+func DifferentWordsCount(str string) int {
+    dict := make(map[string]bool)
+    word := ""
+    result := 0
+    for key, char := range (x + " ") {
+        if unicode.IsLetter(char) {
+            word += string(unicode.ToLower(char))
+        } else if word != "" {
+            if !dict[word] {
+                result += 1
+            }
+            dict[word] = true
+            word = ""
+        }
+    }
+    return result
+} 
